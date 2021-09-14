5422d67d093029212cd733fd8d6f939768bee181
René Preuß <rene@preuss.io>
2021-09-14 21:01:11 +0200

    Update install-dependencies.sh

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -3,5 +3,5 @@
 set -e
 
 apt update
-apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time
+apt install git subversion build-essential python gawk unzip libncurses5-dev zlib1g-dev libssl-dev wget time file
 apt clean
