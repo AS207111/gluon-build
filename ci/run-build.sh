1aba3774d24723d7fd4de1b988b283f9b50dbeef
René Preuß <rene@preuss.io>
2021-09-14 18:46:06 +0200

    Update run-build.sh

--- ci/run-build.sh
+++ ci/run-build.sh
@@ -8,7 +8,10 @@ export GLUON_DEPRECATED=0
 export GLUON_SITEDIR="../gluon-site"
 export GLUON_TARGET=$1
 export BUILD_LOG=1
+export NUM_CORES_PLUS_ONE=$(expr $(nproc) + 1)
 
 cd gluon
 make update
-make -j2 V=s
+make -j$NUM_CORES_PLUS_ONE V=s
+make manifest GLUON_AUTOUPDATER_BRANCH=experimental
+contrib/sign.sh $SECRETKEY output/images/sysupgrade/experimental.manifest
