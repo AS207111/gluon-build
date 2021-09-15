cf3eb069a7b9997306a70a82421ba29b41c36c33
René Preuß <rene@preuss.io>
2021-09-15 12:25:45 +0200

    Update run-build.sh

--- ci/run-build.sh
+++ ci/run-build.sh
@@ -10,6 +10,7 @@ export GLUON_TARGET=$1
 export BUILD_LOG=1
 export NUM_CORES_PLUS_ONE=$(expr $(nproc) + 1)
 
+git clone https://github.com/freifunk-gluon/gluon.git gluon -b v2021.1
 cd gluon
 make update
 make -j$NUM_CORES_PLUS_ONE V=s
