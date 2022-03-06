ff23a766dc69c58d88c565fbd10302e29fcf07d5
René Preuß <rene@preuss.io>
2022-03-06 16:43:58 +0100

    Update run-build.sh

--- ci/run-build.sh
+++ ci/run-build.sh
@@ -7,6 +7,7 @@ export GLUON_AUTOREMOVE=1
 export GLUON_DEPRECATED=0
 export GLUON_SITEDIR="../gluon-site"
 export GLUON_TARGET=$1
+export GLUON_BRANCH=$3
 export BUILD_LOG=1
 export NUM_CORES_PLUS_ONE=$(expr $(nproc) + 1)
 
